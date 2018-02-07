7f537f6d8a4aa0e53b81dc548103dff752e4d645
SimonSUDRE <sta.sudre@gmail.com>
2018-02-07 09:21:36 +0100

    modif nom redirection mvc et ajout controller erreur

--- src/main/java/dev/paie/web/controller/GeneralController.java
+++ src/main/java/dev/paie/web/controller/GeneralController.java
@@ -1,16 +1,13 @@
 package dev.paie.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Controller
-@RequestMapping("/")
+@ControllerAdvice
 public class GeneralController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView creerBulletinForm() {
-		return new ModelAndView("redirect:/mvc/employes/lister");
-	}	
+	@ExceptionHandler(Throwable.class)
+	public String genericErrorPage(Exception e) {
+		return "error";
+	}
 }
